235bb70b32b50ec9fb46db2e4ecd0347608ccadb
Rodrigo Ramírez <ramirezretamalrodrigo@gmail.com>
2019-06-05 22:20:49 -0300

    first commit

---
+++ src/pages/index.paginas.ts
@@ -0,0 +1,4 @@
+export {HomePage} from './home/home.page';
+export {MapasPage} from './mapas/mapas.page';
+export {TabsPage} from './tabs/tabs.page';
+export {GuardadosPage} from './guardados/guardados.page';
